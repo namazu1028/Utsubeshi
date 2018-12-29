46b2c6a5bceb3f9fa8327e736695a18898d28a8b
namazu1028 <45709526+namazu1028@users.noreply.github.com>
2018-12-29 13:47:55 +0900

    敵を作る

---
+++ 2d-shooting-game/Assets/Scripts/Enemy.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    //Spaceshipコンポーネント
+    Spaceship spaceship;
+
+    IEnumerator Start()
+    {
+
+    
+    
+
+
+	
+
+        //Spaceshipコンポーネントを取得
+        spaceship = GetComponent<Spaceship>();
+
+        //ローカル座標のY軸のマイナス方向に移動する
+        spaceship.Move(transform.up * -1);
+
+        //canshotがfalseの場合、ここでコルーチンを終了させる
+        if(spaceship.canshot == false)
+        {
+            yield break;
+        }
+
+        while (true)
+        {
+          //子要素をすべて習得する
+          for(int i = 0;i < transform.childCount; i++)
+            {
+                Transform shotPosition = transform.GetChild(i);
+
+                //shotPositionの位置/角度で弾を撃つ
+                spaceship.Shot(shotPosition);
+
+            }
+            //shotDelay秒待つ
+            yield return new WaitForSeconds(spaceship.shotDelay);
+
+
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- 2d-shooting-game/Assets/Scripts/Player.cs
+++ 2d-shooting-game/Assets/Scripts/Player.cs
@@ -1,24 +1,25 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 
 public class Player : MonoBehaviour {
 
-    //移動スピード
-    public float speed = 5;
+    //Spaceshipコンポーネント
+    Spaceship spaceship;
 
-    //playerBulletプレハブ
-    public GameObject bullet;
+    
 
     //Startメソッドをコルーチンとして呼び出す
-    private IEnumerator Start()
+    IEnumerator Start()
     {
+        //Spaceshipコンポーネントを取得
+        spaceship = GetComponent<Spaceship>();
         while (true)
         {
             //弾をプレイヤーと同じ位置/角度で作成
-            Instantiate(bullet, transform.position, transform.rotation);
-            //0.05秒待つ
-            yield return new WaitForSeconds(0.05f);
+            spaceship.Shot(transform);
+            //shotDelay秒待つ
+            yield return new WaitForSeconds(spaceship.shotDelay);
 
 
 
@@ -40,8 +41,8 @@ public class Player : MonoBehaviour {
         //移動する向きを求める
         Vector2 direction = new Vector2(x, y).normalized;
 
-        //移動する向きとスピードを代入する
-        GetComponent<Rigidbody2D>().velocity = direction * speed;
+        //移動
+        spaceship.Move(direction);
 
 	}
 }
