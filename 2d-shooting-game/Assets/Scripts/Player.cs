16c1b72a5cb7bb30c1d1dfb470fb8bcfb01698b2
namazu1028 <45709526+namazu1028@users.noreply.github.com>
2018-12-29 12:29:22 +0900

    playerから弾を撃つ

--- 2d-shooting-game/Assets/Scripts/Player.cs
+++ 2d-shooting-game/Assets/Scripts/Player.cs
@@ -6,9 +6,30 @@ public class Player : MonoBehaviour {
 
     //移動スピード
     public float speed = 5;
-	
-	// Update is called once per frame
-	void Update () {
+
+    //playerBulletプレハブ
+    public GameObject bullet;
+
+    //Startメソッドをコルーチンとして呼び出す
+    private IEnumerator Start()
+    {
+        while (true)
+        {
+            //弾をプレイヤーと同じ位置/角度で作成
+            Instantiate(bullet, transform.position, transform.rotation);
+            //0.05秒待つ
+            yield return new WaitForSeconds(0.05f);
+
+
+
+
+
+        }  
+    }
+
+
+    // Update is called once per frame
+    void Update () {
 
         //右・左
         float x = Input.GetAxisRaw("Horizontal");
