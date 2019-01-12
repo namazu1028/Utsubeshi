4f3ef87bf3e5c5907c44442202e50db380bf5b4a
namazu1028 <45709526+namazu1028@users.noreply.github.com>
2019-01-12 14:50:53 +0900

    BOOM!!

--- 2d-shooting-game/Assets/Scripts/Enemy.cs
+++ 2d-shooting-game/Assets/Scripts/Enemy.cs
@@ -1,53 +1,59 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class Enemy : MonoBehaviour {
-
-    //Spaceshipコンポーネント
+public class Enemy : MonoBehaviour
+{
+    // Spaceshipコンポーネント
     Spaceship spaceship;
 
     IEnumerator Start()
     {
 
-    
-    
-
-
-	
-
-        //Spaceshipコンポーネントを取得
+        // Spaceshipコンポーネントを取得
         spaceship = GetComponent<Spaceship>();
 
-        //ローカル座標のY軸のマイナス方向に移動する
+        // ローカル座標のY軸のマイナス方向に移動する
         spaceship.Move(transform.up * -1);
 
-        //canshotがfalseの場合、ここでコルーチンを終了させる
-        if(spaceship.canshot == false)
+        // canShotがfalseの場合、ここでコルーチンを終了させる
+        if (spaceship.canShot == false)
         {
             yield break;
         }
 
         while (true)
         {
-          //子要素をすべて習得する
-          for(int i = 0;i < transform.childCount; i++)
+
+            // 子要素を全て取得する
+            for (int i = 0; i < transform.childCount; i++)
             {
+
                 Transform shotPosition = transform.GetChild(i);
 
-                //shotPositionの位置/角度で弾を撃つ
+                // ShotPositionの位置/角度で弾を撃つ
                 spaceship.Shot(shotPosition);
-
             }
-            //shotDelay秒待つ
+
+            // shotDelay秒待つ
             yield return new WaitForSeconds(spaceship.shotDelay);
-
-
         }
+    }
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        // レイヤー名を取得
+        string layerName = LayerMask.LayerToName(c.gameObject.layer);
+
+        // レイヤー名がBullet (Player)以外の時は何も行わない
+        if (layerName != "Bullet (Player)") return;
+
+        // 弾の削除
+        Destroy(c.gameObject);
+
+        // 爆発
+        spaceship.Explosion();
+
+        // エネミーの削除
+        Destroy(gameObject);
+    }
 }
--- 2d-shooting-game/Assets/Scripts/Spaceship.cs
+++ 2d-shooting-game/Assets/Scripts/Spaceship.cs
@@ -1,37 +1,38 @@
 ﻿using UnityEngine;
 
-//Rigidbody2Dコンポーネントを必須にする
 [RequireComponent(typeof(Rigidbody2D))]
-
-public class Spaceship : MonoBehaviour {
-
-    //移動スピード
+public class Spaceship : MonoBehaviour
+{
+    // 移動スピード
     public float speed;
 
-    //弾を撃つ間隔
+    // 弾を撃つ間隔
     public float shotDelay;
 
-    //弾のPrefab
+    // 弾のPrefab
     public GameObject bullet;
 
-    //弾を撃つかどうか
-    public bool canshot;
+    // 弾を撃つかどうか
+    public bool canShot;
 
-    //弾の作成
-    public void Shot (Transform origin)
+    // 爆発のPrefab
+    public GameObject explosion;
+
+    // 爆発の作成
+    public void Explosion()
     {
-        Instantiate(bullet, origin.position, origin.rotation);
-
-
+        Instantiate(explosion, transform.position, transform.rotation);
     }
 
-    //機体の移動
-    public void Move (Vector2 direction)
+    // 弾の作成
+    public void Shot(Transform origin)
+    {
+        Instantiate(bullet, origin.position, origin.rotation);
+    }
+
+    // 機体の移動
+    public void Move(Vector2 direction)
     {
         GetComponent<Rigidbody2D>().velocity = direction * speed;
     }
-
-
-
-	
 }
